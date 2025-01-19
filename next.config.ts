9da86a83e5e1b9d58c4f9acb264d35f932864e4c
Ran Eliyahu <Ran.Eliyahu@ibm.com>
2025-01-19 20:49:56 +0200

    push

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/whattoeat',
+  assetPrefix: '/whattoeat',
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
